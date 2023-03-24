519264038af65bc4c24806d1187e8076bd7bca15
hulihushao <2037499749@qq.com>
2023-03-24 00:45:50 +0000

    Update 5 files

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
-<script setup lang="ts">
-import Layout from "./views/Layout.vue";
-import Aside from "./views/layout/LayoutSider.vue";
-import Header from "./views/layout/LayoutHeader.vue";
-import Content from "./views/layout/LayoutContent.vue";
-import Footer from "./views/layout/LayoutFooter.vue";
+<script setup lang="ts"> 
+import Layout from "./views/Layout.vue"
 </script>
 
 <template>
   <Layout style="height: 100% !important">
-    <template #aside>
-      <Aside />
-    </template>
-    <template #header> <Header /> </template>
-    <template #main> <Content /> </template>
-    <template #footer> <Footer /> </template>
   </Layout>
 </template>
 
