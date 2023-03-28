8c7dc378a4ccff96fe7be59c6a05943da8528bd2
hulihushao <2037499749@qq.com>
2023-03-28 11:23:19 +0000

    Update Welcome.vue

--- src/views/home/Welcome.vue
+++ src/views/home/Welcome.vue
@@ -11,18 +11,25 @@ import Icon from "@/components/icon/Icon.vue";
     <h2>诺依中后台管理系统</h2>
     <p class="tip">系统完善中...</p>
     <br />
-    <p>系统采用<br />vue3+vite+ts+pinia+Antd<br />构建</p>
+    <p>
+      系统采用<br /><a-button type="link">vue3</a-button>+<a-button type="link"
+        >vite</a-button
+      >+<a-button type="link">ts</a-button> +<a-button type="link"
+        >pinia</a-button
+      >
+      +<a-button type="link">Antd</a-button> <br />构建
+    </p>
     <p class="version">version:0.1.0</p>
   </div>
 </template>
 
 <style scoped lang="less">
 #welcome {
-    height:100%;
-    display:flex;
-    align-items:center;
-    flex-direction:column;
-    justify-content:center;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
   p,
   h1,
   h2 {
@@ -32,11 +39,11 @@ import Icon from "@/components/icon/Icon.vue";
     color: #409eff;
     font-size: 40px;
   }
-  .tip{
-    font-size:16px;
+  .tip {
+    font-size: 16px;
   }
-  .version{
-      color:#999;
+  .version {
+    color: #999;
   }
 }
 </style>
