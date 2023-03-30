a9827fd78eb250a0d5724d4384c501123c75aa04
hulihushao <2037499749@qq.com>
2023-03-30 06:47:37 +0000

    Update Breadcrumb.vue

--- src/components/tabs/Breadcrumb.vue
+++ src/components/tabs/Breadcrumb.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
+<div id="breadcrumb-con">
   <a-breadcrumb>
     <a-breadcrumb-item>Home</a-breadcrumb-item>
-    <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
+    <a-breadcrumb-item>Application Center</a-breadcrumb-item>
     <a-breadcrumb-item><a href="">Application List</a></a-breadcrumb-item>
     <a-breadcrumb-item>An Application</a-breadcrumb-item>
   </a-breadcrumb>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="less">
+#breadcrumb-con{
+  width:100%;
+  overflow-x:auto;
+  white-space: nowrap;
+}
+</style>
