a790ecdc39ac68b6f9d626c4f1a65d99fc392769
hulihushao <2037499749@qq.com>
2023-03-30 08:06:21 +0000

    Update 2 files

---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Empty } from "ant-design-vue";
+let simpleImage = ref(Empty.PRESENTED_IMAGE_SIMPLE);
+</script>
+
+<template>
+  <a-empty
+    :image="simpleImage"
+    :image-style="{
+      height: '60px',
+      'margin-top': '100px',
+    }"
+  >
+    <template #description>
+      <span> 敬请期待! </span>
+    </template>
+  </a-empty>
+</template>
+
+<style scoped></style>
