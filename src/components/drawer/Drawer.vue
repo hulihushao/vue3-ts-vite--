72d6ec4846d31f8901a7978fbecc3a0f6f4bde3c
hulihushao <2037499749@qq.com>
2023-04-03 14:51:51 +0000

    Update Drawer.vue

--- src/components/drawer/Drawer.vue
+++ src/components/drawer/Drawer.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import { ref ,toRefs} from "vue";
+import { ref, toRefs } from "vue";
 //接受props
-let visibleObj=defineProps<{
-    visible:{required:true,type:Boolean}
-}>()
+let visibleObj = defineProps<{
+  visible: { required: true; type: Boolean };
+}>();
 //结构保持响应性
-let {visible}=toRefs(visibleObj)
+let { visible } = toRefs(visibleObj);
 //定义派发的事件
-let emits=defineEmits<{
-    (e:"setVisible"):void
-}>()
+let emits = defineEmits<{
+  (e: "setVisible"): void;
+}>();
 
 const afterVisibleChange = (bool: boolean) => {
   console.log("visible", bool);
 };
-
 </script>
 
 <template>
@@ -23,12 +22,19 @@ const afterVisibleChange = (bool: boolean) => {
       v-model:visible="visible"
       class="custom-class"
       style="color: red"
+      :bodyStyle="{ padding: '30px' }"
       title="Basic Drawer"
       placement="right"
       @close="emits('setVisible')"
       @after-visible-change="afterVisibleChange"
     >
-      <p>Some contents...</p>
+      <div>
+        <h3>主题风格设置</h3>
+        <div>
+          <span>主题颜色</span>
+          
+        </div>
+      </div>
       <p>Some contents...</p>
       <p>Some contents...</p>
     </a-drawer>
