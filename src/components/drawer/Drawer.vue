7635b256054fb60400c1649d46d80592231765ad
hulihushao <2037499749@qq.com>
2023-04-04 10:42:20 +0000

    Update 4 files

--- src/components/drawer/Drawer.vue
+++ src/components/drawer/Drawer.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
 import { ref, toRefs } from "vue";
+import { ColorPicker } from "vue3-colorpicker";
+import "vue3-colorpicker/style.css";
+
+import { Sketch } from "@ans1998/vue3-color";
+let colors = ref("");
+let bgColor = ref("#000");
+let showSketch = ref(false);
+function changSketchButton(item) {
+  showSketch.value = false;
+  console.log(item);
+  if (item.isOk) {
+    console.log("确定");
+    bgColor.value = item.activeColor;
+  } else {
+    console.log("取消");
+  }
+}
+const pureColor = ref("#71afe5");
+const gradientColor = ref(
+  "linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 100%)"
+);
+
 //接受props
 let visibleObj = defineProps<{
   visible: { required: true; type: Boolean };
@@ -17,22 +39,44 @@ const afterVisibleChange = (bool: boolean) => {
 </script>
 
 <template>
-  <div id="drawer-con">
+  <div id="drawer-con" @click="showSketch=false">
     <a-drawer
+      :destroyOnClose="true"
       v-model:visible="visible"
       class="custom-class"
-      style="color: red"
+      style=""
       :bodyStyle="{ padding: '30px' }"
-      title="Basic Drawer"
       placement="right"
       @close="emits('setVisible')"
       @after-visible-change="afterVisibleChange"
     >
-      <div>
+      <template #title>
+        <h2>布局设置</h2>
+      </template>
+      <div class="theme-con">
         <h3>主题风格设置</h3>
-        <div>
+        <div :class="$style.color_select">
           <span>主题颜色</span>
-          
+          <div style="position: relative">
+            <div @click.prevent="showSketch = true" :class="$style.color_con"></div>
+            <div style="position: absolute; left:-200%;top:30px">
+              <Sketch
+                v-model="colors"
+                :show="showSketch"
+                @changButton="changSketchButton"
+              ></Sketch>
+            </div>
+          </div>
+          <div></div>
+          <span v-if="false" style="border: 1px solid red; position: relative">
+            <color-picker
+              v-model:pureColor="pureColor"
+              format="rgb"
+              useType="both"
+              pickerType="chrome"
+              v-model:gradientColor="gradientColor"
+            />
+          </span>
         </div>
       </div>
       <p>Some contents...</p>
@@ -41,4 +85,17 @@ const afterVisibleChange = (bool: boolean) => {
   </div>
 </template>
 
-<style scoped></style>
+<style lang="less" module scoped>
+.color_select {
+  width: 100%;
+  padding: 5px 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.color_con {
+  width: 40px;
+  height: 20px;
+  background: v-bind(bgColor);
+}
+</style>
