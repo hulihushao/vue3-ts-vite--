02259fbf75e61a38c10a866f034ef89ad639e105
hulihushao <2037499749@qq.com>
2023-04-09 06:18:57 +0000

    Update theme.ts

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,12 +1,12 @@
 import { defineStore } from "pinia";
-
+import isMobile from "@/utils/deviceType"
 const useTheme = defineStore("theme", {
   state: () => {
     return {
       defaultColor: "#1890ff",
       color: "#1890ff",
       bgImg: "https://img.mp.itc.cn/upload/20161119/1a99a2d9eab34a588f05aaad00ad9883_th.jpg",
-      isTopMenu:true,
+      isTopMenu:isMobile(),
       isShowTime:true,
       isUnfoldMenusSider:false,
       isMenuOpen:false,
