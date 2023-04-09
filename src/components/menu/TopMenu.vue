f3603b2522a4c7b68a9faa1a3064883663b90346
hulihushao <2037499749@qq.com>
2023-04-09 06:18:06 +0000

    Update TopMenu.vue

--- src/components/menu/TopMenu.vue
+++ src/components/menu/TopMenu.vue
@@ -3,7 +3,11 @@ import Menu from "@/components/menu/Menu.vue";
 </script>
 
 <template>
-  <Menu theme="light" mode="horizontal" :style="{border:'1px solid red',width:'40vw'}"/>
+  <Menu
+    theme="light"
+    mode="horizontal"
+    :style="{width: '50vw' }"
+  />
 </template>
 
 <style scoped></style>
