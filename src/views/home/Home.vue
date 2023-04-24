50b004907cf41ee6d1748e82c779a6853992e70b
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-04-24 09:56:21 +0000

    Update 2 files

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -12,19 +12,24 @@ axios.post("https://localhost:5173/queryip").then((res) => {
 <template>
   <div id="home">
     <div class="header">
-      <p class="title">早安，{{ "admin" }}，没好的一天从工作开始！</p>
-      <p class="weather">
-        <span>山东省-烟台市 天气：</span>
-        <span>多云 温度：12摄氏度 风向：北 风力：≤3级 空气湿度：76</span>
-      </p>
-      <p class="count">
-        <Icon icon="UserOutlined"/>
+      <div class="con">
+        <p class="title">早安，{{ "admin" }}，没好的一天从工作开始！</p>
+        <p class="weather">
+          <span>山东省-烟台市 天气：</span>
+          <span>多云 温度：12摄氏度 风向：北 风力：≤3级 空气湿度：76</span>
+        </p>
+        <p class="count">
+          <Icon icon="UserOutlined" />
           <span class="users">用户数 1245</span>
-      </p>
-      <p class="git">
-        <span>Github仓库地址：</span>
-        <a href="" target="_blank">https://github.com/hulihushao/vue3-ts-vite--</a>
-      </p>
+        </p>
+        <p class="git">
+          <span>Github仓库地址：</span>
+          <a href="" target="_blank"
+            >https://github.com/hulihushao/vue3-ts-vite--</a
+          >
+        </p>
+      </div>
+      <div class="img"></div>
     </div>
     <div class="quickly"></div>
     <main class="content"></main>
@@ -41,24 +46,33 @@ axios.post("https://localhost:5173/queryip").then((res) => {
   }
   .header {
     border: 1px solid #ccc;
-    border-radius:5px;
-      padding:10px;
+    border-radius: 5px;
     width: 100%;
     height: auto;
-    .title {
-      font-size: 18px;
-      font-weight: 500;
-    }
-    .weather{
-        padding:10px 0;
-    }
-    .count{
-      .users{
-        margin-left:10px;
+    display: flex;
+    justify-content: space-between;
+    .con {
+      padding: 10px;
+      .title {
+        font-size: 18px;
+        font-weight: 500;
+      }
+      .weather {
+        padding: 10px 0;
+      }
+      .count {
+        .users {
+          margin-left: 10px;
+        }
+      }
+      .git {
+        padding-top: 10px;
       }
     }
-    .git{
-      padding-top:10px;
+    .img {
+      background: url("/static/imgs/dashboard.png") center/cover no-repeat;
+      height: 150px;
+      width: 260px;
     }
   }
   .quickly {
