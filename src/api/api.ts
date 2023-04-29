ca1ce456dd63c058cd92c7502a52c3f3bcabbea7
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-04-29 08:25:26 +0000

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -22,7 +22,7 @@ export class Github {
       "get",
       {
         //解除githubAPI请求次数限制为5000，有效期一个月
-        Authorization: `token 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`
+        Authorization: `token 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`
       }
     );
   }
