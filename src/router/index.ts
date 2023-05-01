b951eccbbc1466cfd1c8659bc01e0e0ccce0c316
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-05-01 06:51:12 +0000

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -62,7 +62,7 @@ const router = createRouter({
 
 import useLoading from "@/store/loading";
 import { storeToRefs } from "pinia";
-//
+//前置守卫
 router.beforeEach((to, from, next) => {
   if (!to.fullPath.includes("/index")) {
     let load = useLoading();
