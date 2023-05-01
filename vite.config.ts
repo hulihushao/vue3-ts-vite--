ba53179715b8fa1f8e1a1c08d23a992d93756431
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-05-01 02:26:01 +0000

    Update 3 files

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import path from "path";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 //这个配置 为了在html中使用 环境变量
-const getViteEnv = (mode:string, target:string) => {
+const getViteEnv = (mode: string, target: string) => {
   return loadEnv(mode, process.cwd())[target];
 };
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base:"/",
+    base: "./",
     plugins: [
       vue(),
       createHtmlPlugin({
@@ -29,7 +29,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-     // open:true,
+      // open:true,
       proxy: {
         "/ws": {
           target: "ws://121.40.165.18:8800", //这里是后台ws访问地址
@@ -38,10 +38,38 @@ export default defineConfig(({ mode }) => {
           rewrite: (path) => path.replace(/^\/ws/, ""),
         },
         "/queryip": {
-          //target: "http://pv.sohu.com/cityjson?ie=utf-8", 
-          target:"http://whois.pconline.com.cn/ipJson.jsp?callback=testJson",
+          //target: "http://pv.sohu.com/cityjson?ie=utf-8",
+          target: "http://whois.pconline.com.cn/ipJson.jsp?callback=testJson",
           changeOrigin: true, //允许跨域设置
           rewrite: (path) => path.replace(/^\/queryip/, ""),
+        },
+      },
+    },
+    build: {
+      outDir: 'dist', // 指定输出路径
+      cssCodeSplit: true, // 启用 CSS 代码拆分
+      sourcemap: false, // 构建后是否生成 source map 文件
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+      rollupOptions: {
+        output: {
+          chunkFileNames: "static/js/[name]-[hash].js",
+          entryFileNames: "static/js/[name]-[hash].js",
+          assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+          manualChunks(id) {
+            //静态资源分拆打包
+            if (id.includes("node_modules")) {
+              return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+            }
+          },
         },
       },
     },
