66c7c3557be089cf63e21897db298f2c09a5bbc8
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-05-09 06:46:45 +0000

    Update Md.vue优化加载数据

--- src/views/study/Md.vue
+++ src/views/study/Md.vue
@@ -1,36 +1,38 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { Component } from "@/api/api";
-import SearchTree from "@/components/tree/SearchTree.vue"
-import type {List_tree} from "@/types/md"
-import useTheme from "@/store/theme"
+import SearchTree from "@/components/tree/SearchTree.vue";
+import type { List_tree } from "@/types/md";
+import useTheme from "@/store/theme";
 
-let themeObj=useTheme()
-
+let themeObj = useTheme();
+let loading = ref(false);
 let list_tree = ref<Array<List_tree>>([]);
 Component.getMd("static/md/index.json").then((res: any) => {
   console.log(res.data);
-  let data:List_tree[]=[]
-  res.data.forEach((item:{name:string},index:number)=>{
+  let data: List_tree[] = [];
+  res.data.forEach((item: { name: string }, index: number) => {
     data.push({
-      title:item.name.split(".")[0],
-      key:index,
-    })
-  })
-  list_tree.value=data
+      title: item.name.split(".")[0],
+      key: index,
+    });
+  });
+  list_tree.value = data;
 });
 
 let md = ref("### 暂无数据");
-let loadMd=(select:string)=>{
-Component.getMd("static/md/"+select+".md").then((res: any) => {
-  console.log(res.data);
-  md.value = res.data;
-});  
-}
+let loadMd = (select: string) => {
+  loading.value = true;
+  Component.getMd("static/md/" + select + ".md").then((res: any) => {
+    console.log(res.data);
+    md.value = res.data;
+    loading.value = false;
+  });
+};
 
-let handleSelect=(selected:List_tree[])=>{
-  loadMd(selected[0].title)
-}
+let handleSelect = (selected: List_tree[]) => {
+  loadMd(selected[0].title);
+};
 </script>
 
 <template>
@@ -38,14 +40,25 @@ let handleSelect=(selected:List_tree[])=>{
     <div class="list-tree">
       目录
       <div>
-        <search-tree :data="list_tree" @select="handleSelect"/>
+        <search-tree :data="list_tree" @select="handleSelect" />
       </div>
     </div>
     <div class="markdown">
-      <v-md-preview
-        :style="{color:themeObj.setColor, background: 'var(--bgColor)', border: '1px solid #999',height:'100%' }"
-        :text="md"
-      ></v-md-preview>
+      <Loading :spinning="loading">
+        <template #tip> 加载中... </template>
+        <template #indicator>
+          <Icon icon="LoadingOutlined" />
+        </template>
+        <v-md-preview
+          :style="{
+            color: themeObj.setColor,
+            background: 'var(--bgColor)',
+            border: '1px solid #999',
+            height: '100%',
+          }"
+          :text="md"
+        ></v-md-preview>
+      </Loading>
     </div>
   </div>
 </template>
@@ -54,7 +67,7 @@ let handleSelect=(selected:List_tree[])=>{
 #markdown-con {
   height: 100%;
   border: 1px solid red;
-  color:v-bind('themeObj.setColor') !important;
+  color: v-bind("themeObj.setColor") !important;
   display: flex;
   .list-tree,
   .markdown {
@@ -64,11 +77,10 @@ let handleSelect=(selected:List_tree[])=>{
   .list-tree {
     width: 30%;
     border: 1px solid red;
-    padding:10px;
+    padding: 10px;
   }
   .markdown {
     width: 100%;
-    
   }
 }
 </style>
