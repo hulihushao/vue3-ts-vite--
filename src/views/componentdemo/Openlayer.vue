847a3befcfcc62b2f6050c0b4664b8d3ad0544aa
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-05-21 06:49:21 +0000

    Update 5 files新增openlayers

---
+++ src/views/componentdemo/Openlayer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Openlayer from "@/views/componentdemo/openlayers/index.vue";
+import DemoContainer from "@/views/componentdemo/index.vue";
+</script>
+
+<template>
+  <demo-container title="Threejs Demo" src="@/views/componentdemo/threejs/index.vue">
+    <Openlayer />
+  </demo-container>
+</template>
+
+<style scoped></style>
---
+++ src/views/componentdemo/openlayers/index.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import "ol/ol.css";
+import * as ol from "ol";
+import olLayerTile from "ol/layer/Tile";
+import olSourceXYZ from "ol/source/XYZ";
+console.log(ol);
+
+const map = ref();
+interface olLayerTileConfig extends AxiosRequestConfig {
+  headers: any;
+}
+let initMap = (dom: HTMLDivElement) => {
+  let layers = [
+    new olLayerTile({
+      title: "天地图矢量图层",
+      zIndex: 1,
+      source: new olSourceXYZ({
+        url:
+          "https://t{0-6}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=" /* TiandituKey */ +
+          "27c199d949d448c75e9a241e996d65da", //TiandituKey为天地图密钥,
+        wrapX: false,
+        projection: "EPSG:3857",
+        crossOrigin: "anonymous",
+      }),
+      isBaseLayer: true,
+    }),
+    new olLayerTile({
+      title: "天地图矢量注记图层",
+      zIndex: 1,
+      source: new olSourceXYZ({
+        url:
+          "https://t{0-6}.tianditu.gov.cn/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=" /* TiandituKey */ +
+          "27c199d949d448c75e9a241e996d65da", //TiandituKey为天地图密钥
+        projection: "EPSG:3857",
+        crossOrigin: "anonymous",
+      }),
+      isBaseLayer: true,
+    }),
+  ];
+  map.value = new ol.Map({
+    layers: layers,
+    view: new ol.View({
+      center: [114.064839, 22.548857], // 中心位置
+      zoom: 10, // 缩放登级
+      maxZoom: 18,
+      projection: "EPSG:4326", // 使用坐标系
+    }),
+    target: dom,
+  });
+};
+onMounted(() => {
+  let container = document.querySelector("#map");
+  initMap(container as HTMLDivElement);
+});
+</script>
+
+<template>
+  <div id="map-con">
+    <div id="map"></div>
+  </div>
+</template>
+
+<style scoped lang="less">
+#map-con {
+  border: 1px solid red;
+  width: 100%;
+  height: 55vh;
+  #map {
+    height: 100%;
+  }
+}
+</style>
